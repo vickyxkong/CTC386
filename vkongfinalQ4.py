2a21bba16751bcc61202db1d9d63ef6df20cd3d0
blueberryyejun <blueberryyejun@gmail.com>
2026-01-29 01:47:46 +0000

    question 4 of final

---
+++ vkongfinalQ4.py
@@ -0,0 +1,18 @@
+#Vicky Kong
+#Final Question 1
+
+#function
+
+def HNY(n):
+    for i in range(n):
+        print("HAPPY NEW YEAR!")
+
+#main
+
+n=int(input("Enter a number between 1 and 10:"))
+while(n<1)or(n>10):
+    print("Your number is not within 1 and 10.")
+    n=int(input("Enter a number between 1 and 10:"))
+if(n>=1)and(n<=10):
+    HNY(n)
+
