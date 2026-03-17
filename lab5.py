cf5d0a9be8fb97431ea944c0883a648e7c0571c2
blueberryyejun <blueberryyejun@gmail.com>
2026-03-17 16:47:54 +0000

    python review

---
+++ lab5.py
@@ -0,0 +1,9 @@
+#Python Review
+
+base=int(input("Enter the base of the rectangle:"))
+height=int(input("Enter the height of the rectangle:"))
+
+area=base*height
+
+print("The area of the rectangle is",area)
+
