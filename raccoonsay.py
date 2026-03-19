178e27577ce806e173e2e4e5d2c5301d7f210d01
blueberryyejun <blueberryyejun@gmail.com>
2026-03-19 01:57:06 +0000

    My attempt to make a raccoon talk.

---
+++ raccoonsay.py
@@ -0,0 +1,16 @@
+#Make your program work similar to the cowsay function by asking the user to input a string and display that string with your best attempt at a text picture of a racoon saying the user's phrase.
+
+sentence=str(input("Enter a sentence:"))
+
+print("----------------------")
+print(sentence)
+print("----------------------")
+print("         \/           ")
+print("   /\          /\     ")
+print("  /  \------- /  \    ")
+print("|                  |  ")
+print("|    O       O     |  ")
+print("|        o         |  ")
+print("|       / \        |  ")
+print(" ------------------   ")
+
